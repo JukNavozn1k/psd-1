cd0d1ee9da0a378ae049a5944b563d45c4da0feb
gleb <deffender1337@gmail.com>
2025-02-11 23:32:11 +0300

    Implement interactive SmallInteger calculator in Main class

--- psd/src/main/java/com/smallinteger/Main.java
+++ psd/src/main/java/com/smallinteger/Main.java
@@ -1,14 +1,53 @@
 package com.smallinteger;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        SmallInteger a = new SmallInteger(10);
-        SmallInteger b = new SmallInteger(5);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("a + b = " + a.add(b));
-        System.out.println("a - b = " + a.subtract(b));
-        System.out.println("a * b = " + a.multiply(b));
-        System.out.println("a / b = " + a.divide(b));
-        System.out.println("a % b = " + a.mod(b));
+        System.out.println("SmallInteger calculator!!!");
+        try {
+
+            System.out.print("Enter first num: ");
+            int firstVal = scanner.nextInt();
+            SmallInteger first = new SmallInteger(firstVal);
+
+            System.out.print("Enter op: (+, -, *, /, %): ");
+            String operator = scanner.next();
+
+            System.out.print("Enter second num: ");
+            int secondVal = scanner.nextInt();
+            SmallInteger second = new SmallInteger(secondVal);
+
+            SmallInteger result;
+            switch (operator) {
+                case "+":
+                    result = first.add(second);
+                    break;
+                case "-":
+                    result = first.subtract(second);
+                    break;
+                case "*":
+                    result = first.multiply(second);
+                    break;
+                case "/":
+                    result = first.divide(second);
+                    break;
+                case "%":
+                    result = first.mod(second);
+                    break;
+                default:
+                    System.out.println("Неизвестный оператор: " + operator);
+                    return;
+            }
+            System.out.println("Результат: " + result);
+        } catch (SmallInteger.SmallIntegerOutOfRangeException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
